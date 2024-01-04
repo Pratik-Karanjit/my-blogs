f129a7c4b2727c0823b8bf8e9281a25f0af3d057
Pratik Karanjit <pratikkaranjit@gmail.com>
2024-01-04 10:07:15 +0545

    add features

--- src/Project Components/BlogTwo.jsx
+++ src/Project Components/BlogTwo.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css'
 
+//blogTwo goes here
 const BlogTwo = () => {
   return (
     <div className="container mt-5">
